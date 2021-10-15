a485c6b2422a0724a261cd242cbecfe9f5eb6410
shah-krupal <77288288+shah-krupal@users.noreply.github.com>
2021-10-15 10:38:23 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Tasks from './components/Tasks';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Head">
+      <Header title='Hello from a title'/>
+      <Tasks/>
+      console.log(task[1])
+      <h1>Hello from JS</h1>
     </div>
   );
 }
 
-export default App;
+
+export default Header;
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Button from './Button'
+
+const Header = (props) => {
+    const onClick = () => {
+        console.log('Its click')
+    }
+    return (
+        <header>
+            <h1><b><i>Header.js {props.title}</i></b></h1>
+            <Button color='#48A14D' text='ADD to CART' onClick={onClick}/>
+        </header>
+    )
+}
+
+
+export default Header
