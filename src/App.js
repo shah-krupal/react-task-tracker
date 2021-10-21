87f83c573a24db0e04651c81cb9cb81f9c52b682
shah-krupal <77288288+shah-krupal@users.noreply.github.com>
2021-10-21 22:58:13 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -5,16 +5,22 @@ import SearchBar from './components/SearchBar';
 import Header from './components/Header';
 import Button from './components/Button';
 import TeachState from './components/TeachState';
+import {useState} from 'react'
 
 
 function App() 
 {
+  const [data,setData] = useState({})
+  const updateData = (searchParameters) => {
+    setData(searchParameters) ;
+  }
   return (
     <div className="Head">
       
       <Header/>
       <Button/>
-      <SearchBar/>
+      <SearchBar callback = {updateData}/>
+      <p>"name:" {"name" in data ? data["name"] : "No display" }</p>
       <TeachState/>
     </div>
   );
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,26 +1,40 @@
 import React from 'react'
+import { useState } from 'react'
 
+const SearchBar = (props) => {
+    const [name,setName] = useState("") ;
+    const [price,setPrice] = useState(0) ;
+    const [type,setType] = useState("") ;
+    const [brand,setBrand] = useState("") ;
 
-const SearchBar = () => {
+    const searchButtonPressed = () => {
+        {/*console.log(name) ;
+        console.log(price) ;
+        console.log(type) ;
+        console.log(brand) ;*/}
+        props.callback({name:name, price:price, type:type,brand:brand}) ;
+    };
+
     return (
         <div>
             <h2>Search for an Item</h2>
             <form>
-                <label for="name-field">Name Search</label>
-                <input id="name-field" type = "text" />
+                <label htmlFor="name-field">Name Search</label>
+                <input id="name-field" type = "text"  value = {name} onChange = { (e) => setName(e.target.value)}/>
                 <br/>
-                <label for="price-field">Price Search</label>
-                <input id="price-field" type = "number" />
+                <label htmlFor="price-field">Price Search</label>
+                <input id="price-field" type = "number" value = {price} onChange = { (e) => setPrice(e.target.value)} />
                 <br/>
-                <label for="type-field">Type Search</label>
-                <input id="type-field" type = "text" />
+                <label htmlFor="type-field">Type Search</label>
+                <input id="type-field" type = "text" value = {type} onChange = { (e) => setType(e.target.value)}/>
                 <br/>
-                <label for="brand-field">Brand Search</label>
-                <input id="brand-field" type = "text" />
+                <label htmlFor="brand-field">Brand Search</label>
+                <input id="brand-field" type = "text" value = {brand} onChange = { (e) => setBrand(e.target.value)}/>
                 <br/>
-                <button>Search</button>
+                <button type = "button" onClick = { searchButtonPressed }>Search</button>
                 
             </form> 
+
         </div>
     )
 }
